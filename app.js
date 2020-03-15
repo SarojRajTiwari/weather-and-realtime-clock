8eceba6425e2c6b1b09d544dfd8635f6bf3e4480
SarojRajTiwari <55675698+SarojRajTiwari@users.noreply.github.com>
2020-03-15 15:34:51 +0545

    Add files via upload

---
+++ app.js
@@ -0,0 +1,251 @@
+
+/*function showTime(){
+    var date = new Date();
+    var h = date.getHours(); 
+    var m = date.getMinutes(); 
+    var s = date.getSeconds(); 
+    var session = "AM";
+    
+    if(h == 0){
+        h = 12;
+    }
+    
+    if(h > 12){
+        h = h - 12;
+        session = "PM";
+    }
+    
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+    
+    var time = h + ":" + m + ":" + s + " " + session;
+    document.getElementById("DigitalCLOCK").innerText = time;
+    document.getElementById("DigitalCLOCK").textContent = time;
+    
+    setTimeout(showTime, 1000);
+    
+}
+ 
+showTime();*/
+const iconElement = document.querySelector(".weather-icon");
+const tempElement = document.querySelector(".temperature-value p");
+const descElement = document.querySelector(".temperature-description p");
+const locationElement = document.querySelector(".location p");
+const notificationElement = document.querySelector(".notification");
+
+// App data
+const weather = {};
+
+weather.temperature = {
+    unit : "celsius"
+}
+
+// APP CONSTS AND VARS
+const KELVIN = 273;
+// API KEY
+const key = "82005d27a116c2880c8f0fcb866998a0";
+
+// CHECK IF BROWSER SUPPORTS GEOLOCATION
+if('geolocation' in navigator){
+    navigator.geolocation.getCurrentPosition(setPosition, showError);
+}else{
+    notificationElement.style.display = "block";
+    notificationElement.innerHTML = "<p>Browser doesn't Support Geolocation</p>";
+}
+
+// SET USER'S POSITION
+function setPosition(position){
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+    
+    getWeather(latitude, longitude);
+}
+
+// SHOW ERROR WHEN THERE IS AN ISSUE WITH GEOLOCATION SERVICE
+function showError(error){
+    notificationElement.style.display = "block";
+    notificationElement.innerHTML = `<p> ${error.message} </p>`;
+}
+
+// GET WEATHER FROM API PROVIDER
+function getWeather(latitude, longitude){
+    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+    
+    
+    fetch(api)
+        .then(function(response){
+            let data = response.json();
+            return data;
+        })
+        .then(function(data){
+            weather.temperature.value = Math.floor(data.main.temp - KELVIN);
+            weather.description = data.weather[0].description;
+            weather.iconId = data.weather[0].icon;
+            weather.city = data.name;
+            weather.country = data.sys.country;
+        })
+        .then(function(){
+            displayWeather();
+        });
+}
+
+// DISPLAY WEATHER TO UI
+function displayWeather(){
+    iconElement.innerHTML = `<img src="icons/${weather.iconId}.png"/>`;
+    tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+    descElement.innerHTML = weather.description;
+    locationElement.innerHTML = `${weather.city}, ${weather.country}`;
+}
+
+// C to F conversion
+function celsiusToFahrenheit(temperature){
+    return (temperature * 9/5) + 32;
+}
+
+// WHEN THE USER CLICKS ON THE TEMPERATURE ELEMENET
+tempElement.addEventListener("click", function(){
+    if(weather.temperature.value === undefined) return;
+    
+    if(weather.temperature.unit == "celsius"){
+        let fahrenheit = celsiusToFahrenheit(weather.temperature.value);
+        fahrenheit = Math.floor(fahrenheit);
+        
+        tempElement.innerHTML = `${fahrenheit}°<span>F</span>`;
+        weather.temperature.unit = "fahrenheit";
+    }else{
+        tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+        weather.temperature.unit = "celsius"
+    }
+});
+
+function clock(){
+    //calculate angle
+
+    var d = new Date();
+      
+      var date = d.getDate();
+      
+      var month = d.getMonth();
+      var montharr =["Jan","Feb","Mar","April","May","June","July","Aug","Sep","Oct","Nov","Dec"];
+      month=montharr[month];
+      
+      var year = d.getFullYear();
+      
+      var day = d.getDay();
+      var dayarr =["Sunday","Monday","Tuesday","wednesday","thursday","friday","satdurday"];
+      day=dayarr[day];
+      
+      //var hour =d.getHours();
+      //var min = d.getMinutes();
+      //var sec = d.getSeconds();
+    
+      document.getElementById("date").innerHTML=day+" "+date+" "+month+" "+year;
+    var d, h, m, s;
+    d = new Date;
+    
+    h = 30 * ((d.getHours() % 12) + d.getMinutes() / 60);
+    m = 6 * d.getMinutes();
+    s = 6 * d.getSeconds();
+    
+    //move hands
+    setAttr('h-hand', h);
+    setAttr('m-hand', m);
+    setAttr('s-hand', s);
+    setAttr('s-tail', s+180);
+    
+    //display time
+    h = d.getHours();
+    m = d.getMinutes();
+    s = d.getSeconds();
+    
+    if(h >= 12){
+        setText('suffix', 'PM');
+    }else{
+        setText('suffix', 'AM');
+    }
+    
+    if(h != 12){
+        h %= 12;
+    }
+    
+    setText('sec', s);
+    setText('min', m);
+    setText('hr', h);
+    
+    //call every second
+    setTimeout(clock, 1000);
+    
+};
+
+function setAttr(id,val){
+    var v = 'rotate(' + val + ', 70, 70)';
+    document.getElementById(id).setAttribute('transform', v);
+};
+
+function setText(id,val){
+    if(val < 10){
+        val = '0' + val;
+    }
+    document.getElementById(id).innerHTML = val;
+};
+
+window.onload=clock;
+
+
+window.onload = setInterval(clock,1000);
+
+   
+/*function renderTime(){
+    var mydate = new Date();
+    var year=mydate.getyear();
+    if(year<1000){
+        year+=1900
+
+    }
+    var day = mydate.getday();
+    var month = mydate.getmonth();
+    var daym= mydate.getdate();
+    var dayarray = new Array ("sunday","monday","tuesday","wednesday","thursday","friday","satdurday");
+    var montharray= new Array("janary","feb","april","march");
+    var myclock=document.getElementById("clockDisplay");
+
+    myClock.textContent=""dayarray[day]+""+daym+""+montharray[month]+""+year;
+
+    setTimeout("renderTime()");
+}
+
+    renderTime();
+
+
+
+// SELECT ELEMENTS
+/*function showTime(){
+    var date = new Date();
+    var h = date.getHours(); 
+    var m = date.getMinutes(); 
+    var s = date.getSeconds(); 
+    var session = "AM";
+    
+    if(h == 0){
+        h = 12;
+    }
+    
+    if(h > 12){
+        h = h - 12;
+        session = "PM";
+    }
+    
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+    
+    var time = h + ":" + m + ":" + s + " " + session;
+    document.getElementById("DigitalCLOCK").innerText = time;
+    document.getElementById("DigitalCLOCK").textContent = time;
+    
+    setTimeout(showTime, 1000);
+    
+}
+ 
+showTime();*/
